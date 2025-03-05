2002601ff3343e11e1c248de53e891dc98245d6f
Moikelaff <dylan.adiprawira@gmail.com>
2025-03-05 12:47:38 +0700

    feat: added halyard font on config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,12 +5,16 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.css",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        halyard: ["var(--font-halyard)"],
       },
     },
   },
