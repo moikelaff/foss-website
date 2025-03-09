f9d8c07d633261e3281288443b60a1d3b9da491a
haninrahmania <hanin.atina@ui.ac.id>
2025-03-09 19:37:02 +0700

    Finished Footer component

--- src/app/admissions/page.tsx
+++ src/app/admissions/page.tsx
@@ -3,16 +3,21 @@ import React from "react";
 import AdmissionsHero from "@/components/AdmissionsHero";
 import ProgramsSection from "@/components/ProgramsSection";
 import FAQSection from "@/components/FAQSection";
+import Footer from "@/components/Footer";
 
 export default function AdmissionsPage() {
   return (
     // tinggal tambahin navbar here
-    <main>
-      <AdmissionsHero />
-      <ProgramsSection />
-      <FAQSection />
-      
-    </main>
-    // tinggal tambahin footer here
+    <>
+      // tinggal tambahin navbar here
+      <main>
+        <AdmissionsHero />
+        <ProgramsSection />
+        <FAQSection />
+
+      </main>
+      <footer>
+        <Footer />
+      </footer></>
   );
 }
